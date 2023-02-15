c0fc3b4c207bf9ba16593819d12bb5b13a2e0c3a
daratyoon <sandratsaiiyun@yahoo.com>
2023-02-15 11:40:14 -0600

    Imported moment.js and timezone libraries

--- src/script.js
+++ src/script.js
@@ -3,3 +3,5 @@ let newYorkElement = document.querySelector("#new-york");
 let newYorkDateElement = newYorkElement.querySelector(".date");
 let newYorkTimeElement = newYorkElement.querySelector(".time");
 let newYorkTime = moment().tz("America/New York");
+
+newYorkElement.innerHTML = newYorkTime.format(DD, MM);
