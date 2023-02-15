045d8b3b54a9729e24e538b36cb7ab90d327a932
daratyoon <sandratsaiiyun@yahoo.com>
2023-02-15 10:53:44 -0600

    Created JS variables for New York

---
+++ src/script.js
@@ -0,0 +1,5 @@
+// New York
+let newYorkElement = document.querySelector("#new-york");
+let newYorkDateElement = newYorkElement.querySelector(".date");
+let newYorkTimeElement = newYorkElement.querySelector(".time");
+let newYorkTime = moment().tz("America/New York");
